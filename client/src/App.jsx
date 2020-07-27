6548a155e08687edad1ba36c16a70969760d785e
juanb86 <juan.e.begnis@gmail.com>
2020-07-27 13:23:42 -0300

    Load User & Set Token

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,12 @@ import Alerts from "./components/layout/Alerts";
 import ContactState from "./context/contact/ContactState";
 import AuthState from "./context/auth/AuthState";
 import AlertState from "./context/alert/AlertState";
+import setAuthToken from "./utils/setAuthToken";
 import "./App.css";
+
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
 
 const App = () => {
   return (
@@ -20,13 +25,13 @@ const App = () => {
           <Router>
             <Fragment>
               <Navbar />
-              <div className='container'>
+              <div className="container">
                 <Alerts />
                 <Switch>
-                  <Route exact path='/' component={Home} />
-                  <Route exact path='/about' component={About} />
-                  <Route exact path='/register' component={Register} />
-                  <Route exact path='/login' component={Login} />
+                  <Route exact path="/" component={Home} />
+                  <Route exact path="/about" component={About} />
+                  <Route exact path="/register" component={Register} />
+                  <Route exact path="/login" component={Login} />
                 </Switch>
               </div>
             </Fragment>
